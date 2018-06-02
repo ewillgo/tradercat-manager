fbd9ba2b3ac9aa9c60ac9b0df1e3485c8608fcd1
ewill.leung@foxmail.com <will89liangwenwei>
2018-06-02 17:46:36 +0800

    ...

--- src/libs/constant.js
+++ src/libs/constant.js
@@ -4,10 +4,10 @@ Constant.USER = {};
 
 Constant.USER.PASSWORD_LENGTH = 8;
 
-Constant.USER.LOGIN_CAPTCHA_URL = "http://manager.sportsdb.cc:9001/user/loginCaptcha";
-Constant.USER.REGISTER_CAPTCHA_URL = "http://manager.sportsdb.cc:9001/user/registerCaptcha";
-Constant.USER.REGISTER_URL = "http://manager.sportsdb.cc:9001/user/register";
-Constant.USER.LOGIN_URL = "http://manager.sportsdb.cc:9001/user/login";
+Constant.USER.LOGIN_CAPTCHA_URL = "http://manager-backend.tradercat.net/user/loginCaptcha";
+Constant.USER.REGISTER_CAPTCHA_URL = "http://manager-backend.tradercat.net/user/registerCaptcha";
+Constant.USER.REGISTER_URL = "http://manager-backend.tradercat.net/user/register";
+Constant.USER.LOGIN_URL = "http://manager-backend.tradercat.net/user/login";
 
 Constant.USER.LOGIN_LOADING = "正在登录，请稍后...";
 Constant.USER.REGISTER_LOADING = "正在注册，请稍后...";
